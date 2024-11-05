fd5e868f1f035e051abec55c050bc5fa7a01c388
miss-gif <svx327@naver.com>
2024-11-05 20:11:46 +0900

    Remove unused SVG assets: Figma.svg, Regex-Dark.svg, and Regex-Light.svg

--- src/components/about/SkillSet.tsx
+++ src/components/about/SkillSet.tsx
@@ -3,22 +3,38 @@ import {
   Bodies,
   Composite,
   Engine,
+  Events,
   Mouse,
   MouseConstraint,
   Render,
   Runner,
 } from 'matter-js'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
+import AXIOS from '@assets/skill_icons/axios.svg'
+import JS from '@assets/skill_icons/css.svg'
+
+const data = {
+  자바스크립트: { title: JS, level: 4, description: '자바스크립트 설명' },
+  CSS: { title: JS, level: 4, description: 'CSS 설명' },
+  axios: { title: JS, level: 4, description: 'CSS 설명' },
+}
 
 const SkillSet = () => {
+  const [selected, setSelected] = useState(data['CSS'])
   const canvasRef = useRef(null)
 
   useEffect(() => {
     const canvas = canvasRef.current as HTMLCanvasElement | null
     if (!canvas) return
 
+    let observer
+
     const cw = 1000
     const ch = 1000
+
+    const garvityPower = 0.5
+    let gravityDeg = 0
 
     let engine: Engine,
       render: Render,
@@ -52,6 +68,27 @@ const SkillSet = () => {
         },
       })
       Composite.add(engine.world, mouseConstraint)
+
+      canvas.removeEventListener('wheel', mouse.mousewheel)
+      canvas.removeEventListener('DOMMouseScroll', mouse.mousewheel)
+    }
+    const initIntersectionObserver = () => {
+      const options = {
+        threshold: 0.5,
+      }
+      observer = new IntersectionObserver((entries) => {
+        const entry = entries[0]
+        console.log(entry.isIntersecting)
+        if (entry.isIntersecting) {
+          runner.enabled = true
+          Render.run(render)
+        } else {
+          runner.enabled = false
+          Render.stop(render)
+        }
+      }, options)
+
+      observer.observe(canvas)
     }
     const initGround = () => {
       const segment = 32
@@ -70,6 +107,28 @@ const SkillSet = () => {
         })
       }
     }
+    const initSkillIcon = () => {
+      addRect(cw / 2, ch / 2, 100 * 0.7, 100 * 0.7, {
+        label: 'axios',
+        chamfer: { radius: 20 },
+        render: {
+          sprite: {
+            texture: AXIOS,
+            xScale: 0.5,
+            yScale: 0.5,
+          },
+        },
+      })
+      addRect(cw / 2, ch / 2, 100 * 0.7, 100 * 0.7, {
+        render: {
+          sprite: {
+            texture: JS,
+            xScale: 0.5,
+            yScale: 0.5,
+          },
+        },
+      })
+    }
     const addRect = (
       x: number,
       y: number,
@@ -84,12 +143,34 @@ const SkillSet = () => {
     initScene()
     initMouse()
     initGround()
+    initSkillIcon()
+    initIntersectionObserver()
 
-    canvas?.addEventListener('wheel', () => {
-      addRect(mouse.position.x, mouse.position.y, 50, 50)
+    Events.on(mouseConstraint, 'mousedown', () => {
+      const newSelected =
+        mouseConstraint.body && data[mouseConstraint.body.label]
+
+      newSelected && setSelected(newSelected)
+      console.log(mouseConstraint.body)
     })
 
-    return () => {}
+    Events.on(runner, 'tick', () => {
+      gravityDeg += 1
+      engine.world.gravity.x =
+        Math.cos((Math.PI / 100) * gravityDeg) * garvityPower
+      engine.world.gravity.y =
+        Math.sin((Math.PI / 100) * gravityDeg) * garvityPower
+    })
+
+    return () => {
+      observer.unobserve(canvas)
+
+      Composite.clear(engine.world)
+      Mouse.clearSourceEvents(mouse)
+      Render.stop(render)
+      Runner.stop(runner)
+      Engine.clear(engine)
+    }
   }, [])
 
   return (
@@ -97,31 +178,24 @@ const SkillSet = () => {
       <div className="flex items-center">
         <CanvasStyled ref={canvasRef}></CanvasStyled>
         <AsideStyled>
-          <h4 className="text-4xl">자바스크립트</h4>
+          <h4 className="text-4xl">{selected.title}</h4>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis
-            consequuntur ipsa numquam laborum voluptatum, dolores sed odio
-            consectetur illum consequatur optio veniam quasi cumque? Molestiae
-            veritatis maxime qui numquam laborum! Lorem ipsum, dolor sit amet
-            consectetur adipisicing elit. Reiciendis consequuntur ipsa numquam
-            laborum voluptatum, dolores sed odio consectetur illum consequatur
-            optio veniam quasi cumque? Molestiae veritatis maxime qui numquam
-            laborum! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Reiciendis consequuntur ipsa numquam laborum voluptatum, dolores sed
-            odio consectetur illum consequatur optio veniam quasi cumque?
-            Molestiae veritatis maxime qui numquam laborum! Lorem ipsum, dolor
-            sit amet consectetur adipisicing elit. Reiciendis consequuntur ipsa
-            numquam laborum voluptatum, dolores sed odio consectetur illum
-            consequatur optio veniam quasi cumque? Molestiae veritatis maxime
-            qui numquam laborum! Lorem ipsum, dolor sit amet consectetur
-            adipisicing elit. Reiciendis consequuntur ipsa numquam laborum
-            voluptatum, dolores sed odio consectetur illum consequatur optio
-            veniam quasi cumque? Molestiae veritatis maxime qui numquam laborum!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis
-            consequuntur ipsa numquam laborum voluptatum, dolores sed odio
-            consectetur illum consequatur optio veniam quasi cumque? Molestiae
-            veritatis maxime qui numquam laborum!
+            {Array(5)
+              .fill(null)
+              .map((_, i) => {
+                return (
+                  <span
+                    key={i}
+                    style={{
+                      filter: `grayscale(${selected.level <= i ? 1 : 0})`,
+                    }}
+                  >
+                    &#11088;
+                  </span>
+                )
+              })}
           </p>
+          <p>{selected.description}</p>
         </AsideStyled>
       </div>
     </div>
