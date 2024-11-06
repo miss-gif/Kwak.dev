164e544a7fc5c2dcb12d59c4dc04d421d842ff76
miss-gif <svx327@naver.com>
2024-11-06 14:33:37 +0900

    Add: 인터뷰 질문을 데이터로 변환하여 InterviewPage 컴포넌트에 적용, 교육 데이터에 로고 추가

--- src/components/about/Career.tsx
+++ src/components/about/Career.tsx
@@ -1,134 +1,69 @@
+import { education } from '@/data'
+import styled from '@emotion/styled'
+
 const Career = () => {
+  // education 데이터에서 training 배열만 가져오기
+  const trainingData = education[1].training
+
   return (
-    <div className="p-6 lg:p-12 ">
-      <div className="flex flex-col gap-16">
-        <div className="flex flex-col gap-12">
-          <div className="flex flex-col md:flex-row gap-8 md:gap-12">
-            <div className="w-[300px] flex  justify-center">
-              <div className="w-[230px] h-[230px] border-4  rounded-full flex items-center justify-center shadow-md ">
-                <p className="text-lg  font-semibold">로고</p>
+    <ul className="flex flex-col gap-32">
+      {trainingData?.map((training, index) => (
+        <li key={index} className="flex gap-12">
+          {/* 기업로고 영역 */}
+          <div>
+            <LogoStyled>
+              <img src={training.logo} alt={`${training.institution} logo`} />
+            </LogoStyled>
+          </div>
+
+          {/* 활동내역 영역 */}
+          <div className="border-l-4 border-gray-100 pl-12 py-4">
+            <div>
+              <div className="flex flex-col gap-2 mb-4">
+                <h4 className="text-2xl font-semibold">
+                  {training.institution}
+                </h4>
+                <time>{training.duration}</time>
+                <p className="leading-relaxed">{training.course}</p>
               </div>
-            </div>
-            <div className="border-l-4 border-gray-200 pl-8">
-              <div className="커리어">
-                <h4 className="text-2xl font-semibold  mb-2">그린아트학원</h4>
-
-                <p className="">2023.02 - 2023.11</p>
-                <p className=" mb-4 mt-2 leading-relaxed">
-                  기업 요구를 반영한 프로젝트 중심 프론트엔드 React(리액트)
-                  개발자 양성 과정
-                </p>
-
-                <div className="flex flex-col gap-10 mt-10">
-                  <div className="space-y-6">
-                    <div>
-                      <p className="font-semibold ">
-                        주문이요 (음식주문 플랫폼) 협업 3차 프로젝트
-                      </p>
-                      <p className="">2023.07 - 2023.09</p>
-                      <p className=" mt-2 leading-relaxed">
-                        음식 주문 플랫폼을 개발하며 프로젝트를 총괄하고 UI/UX
-                        디자인, 공통 컴포넌트 및 레이아웃 설계, 라우터 설정,
-                        깃허브 관리, 사용자 기능 등을 담당했습니다. Redux
-                        Toolkit을 사용한 상태 관리와 MUI를 활용한 UI 컴포넌트
-                        커스터마이징 경험을 얻었습니다.
-                      </p>
-                      <p className=" leading-relaxed">
-                        프로덕션 배포 이후에는 LaunchDarkly를 활용한
-                        Feature-Flag 관리로, 프로덕션 환경에서 내부 테스트를
-                        진행했습니다.
+              <div className="flex flex-col gap-10">
+                {training.projects.map((project, projIndex) => (
+                  <div key={projIndex}>
+                    <div className="flex flex-col gap-3">
+                      <h5 className="font-semibold">✨ {project.title}</h5>
+                      <time>{project.date}</time>
+                      <p className="leading-6">{project.description}</p>
+                      <p className="leading-relaxed">
+                        기술 스택: {project.techStack.join(', ')}
                       </p>
                     </div>
                   </div>
-                  <div className="space-y-6">
-                    <div>
-                      <p className="font-semibold ">
-                        주문이요 (음식주문 플랫폼) 협업 3차 프로젝트
-                      </p>
-                      <p className="">2023.07 - 2023.09</p>
-                      <p className=" mt-2 leading-relaxed">
-                        음식 주문 플랫폼을 개발하며 프로젝트를 총괄하고 UI/UX
-                        디자인, 공통 컴포넌트 및 레이아웃 설계, 라우터 설정,
-                        깃허브 관리, 사용자 기능 등을 담당했습니다. Redux
-                        Toolkit을 사용한 상태 관리와 MUI를 활용한 UI 컴포넌트
-                        커스터마이징 경험을 얻었습니다.
-                      </p>
-                      <p className=" leading-relaxed">
-                        프로덕션 배포 이후에는 LaunchDarkly를 활용한
-                        Feature-Flag 관리로, 프로덕션 환경에서 내부 테스트를
-                        진행했습니다.
-                      </p>
-                    </div>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row gap-8 md:gap-12">
-            <div className="w-[300px] flex  justify-center">
-              <div className="w-[230px] h-[230px] border-4 rounded-full flex items-center justify-center shadow-md">
-                <p className="text-lg  font-semibold">로고</p>
-              </div>
-            </div>
-            <div className="border-l-4 border-gray-200 pl-8">
-              <div className="커리어">
-                <h4 className="text-2xl font-semibold  mb-2">그린아트학원</h4>
-
-                <p className="">2023.02 - 2023.11</p>
-                <p className=" mb-4 mt-2 leading-relaxed">
-                  기업 요구를 반영한 프로젝트 중심 프론트엔드 React(리액트)
-                  개발자 양성 과정
-                </p>
-
-                <div className="flex flex-col gap-10 mt-10">
-                  <div className="space-y-6">
-                    <div>
-                      <p className="font-semibold ">
-                        주문이요 (음식주문 플랫폼) 협업 3차 프로젝트
-                      </p>
-                      <p className="">2023.07 - 2023.09</p>
-                      <p className=" mt-2 leading-relaxed">
-                        음식 주문 플랫폼을 개발하며 프로젝트를 총괄하고 UI/UX
-                        디자인, 공통 컴포넌트 및 레이아웃 설계, 라우터 설정,
-                        깃허브 관리, 사용자 기능 등을 담당했습니다. Redux
-                        Toolkit을 사용한 상태 관리와 MUI를 활용한 UI 컴포넌트
-                        커스터마이징 경험을 얻었습니다.
-                      </p>
-                      <p className=" leading-relaxed">
-                        프로덕션 배포 이후에는 LaunchDarkly를 활용한
-                        Feature-Flag 관리로, 프로덕션 환경에서 내부 테스트를
-                        진행했습니다.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="space-y-6">
-                    <div>
-                      <p className="font-semibold ">
-                        주문이요 (음식주문 플랫폼) 협업 3차 프로젝트
-                      </p>
-                      <p className="">2023.07 - 2023.09</p>
-                      <p className=" mt-2 leading-relaxed">
-                        음식 주문 플랫폼을 개발하며 프로젝트를 총괄하고 UI/UX
-                        디자인, 공통 컴포넌트 및 레이아웃 설계, 라우터 설정,
-                        깃허브 관리, 사용자 기능 등을 담당했습니다. Redux
-                        Toolkit을 사용한 상태 관리와 MUI를 활용한 UI 컴포넌트
-                        커스터마이징 경험을 얻었습니다.
-                      </p>
-                      <p className=" leading-relaxed">
-                        프로덕션 배포 이후에는 LaunchDarkly를 활용한
-                        Feature-Flag 관리로, 프로덕션 환경에서 내부 테스트를
-                        진행했습니다.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        </li>
+      ))}
+    </ul>
   )
 }
 
 export default Career
+
+const LogoStyled = styled.div`
+  width: 230px;
+  height: 230px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+
+  img {
+    width: 200px;
+    height: 200px;
+    object-fit: contain;
+  }
+`
--- src/pages/InterviewPage.tsx
+++ src/pages/InterviewPage.tsx
@@ -1,45 +1,17 @@
 import Section from '@/components/sections/Section'
+import interviewQuestions from '@/data/interviewQuestions'
 
 const InterviewPage = () => {
   return (
     <Section>
-      <div className="flex flex-col py-6">
-        <div className="mb-4 p-4 bg-gray-100 rounded shadow">
-          <h2 className="text-xl font-bold mb-2">
-            Q. 프론트엔드로 전향한 이유?
-          </h2>
-          <p>
-            웹 디자이너로 활동하며 사용자 중심 디자인, 협업, 프론트엔드 기초
-            지식을 쌓았습니다. 사용자와의 소통에 대해 중요성을 인지하고, 더
-            밀접하게 상호작용 가능한 프로젝트를 개발하고자 프론트엔드 개발자로
-            전향을 결심하게 되었습니다. React, Styled Components, Redux Toolkit
-            등 다양한 프레임워크와 라이브러리를 도입해 발전할 수 있는 새로운
-            도전에 나서고 있습니다.
-          </p>
-        </div>
-        <div className="mb-4 p-4 bg-gray-100 rounded shadow">
-          <h2 className="text-xl font-bold mb-2">
-            Q. 일에 있어 가장 중요하게 생각하는 것이 있다면?
-          </h2>
-          <p>
-            항상 역지사지 마인드로 사용자 중심 개발을 추구합니다. 직관적이고
-            친숙한 UI 제공과 Chrome Lighthouse를 활용해 성능 최적화에
-            신경쓰면서, 다양한 사용자들이 편리하게 서비스를 이용 할 수 있도록
-            기여하고자 합니다.
-          </p>
-        </div>
-        <div className="mb-4 p-4 bg-gray-100 rounded shadow">
-          <h2 className="text-xl font-bold mb-2">
-            Q. 자기계발을 위해 어떤 것들을 해왔는지?
-          </h2>
-          <p>
-            지속적인 학습을 위해 스터디와 온라인 강의에 적극 참여하고, 낯선
-            기술에도 포기 하지 않고 메타인지 방식을 활용해 문제 해결 능력을 쌓고
-            있습니다. 열린 마음으로 피드백을 소중히 여기고, 개발 과정에서 항상
-            개선할 점을 찾기 위해 노력하고 있습니다.
-          </p>
-        </div>
-      </div>
+      <ul className="flex flex-col py-6">
+        {interviewQuestions.map((question, index) => (
+          <li key={index} className="mb-4 p-4 bg-gray-100 rounded shadow">
+            <h3 className="text-xl font-bold mb-2">Q. {question.question}</h3>
+            <p>{question.answer}</p>
+          </li>
+        ))}
+      </ul>
     </Section>
   )
 }
