8612aeb7b7d0194990e1b29eef6f30e7e7d60ef7
miss-gif <svx327@naver.com>
2024-11-09 12:43:45 +0900

    Fix : 배포 에러 해결

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -1,17 +1,4 @@
-import { linkItems } from "@/mocks/data";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-
 const PageTitle = ({ children }: { children: React.ReactNode }) => {
-  const location = useLocation();
-  const [currentName, setCurrentName] = useState("");
-
-  useEffect(() => {
-    const currentPath = location.pathname.replace("/", "");
-    const matchedItem = linkItems.find((item) => item.path === currentPath);
-    setCurrentName(matchedItem ? matchedItem.name : "프로젝트");
-  }, [location.pathname]);
-
   return (
     <div>
       <img
