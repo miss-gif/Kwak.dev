50a9c20854a4feff9bc78a9fe990b35aa6c8c259
miss-gif <svx327@naver.com>
2024-12-03 22:04:17 +0900

    Refactor: 헤더 관련 컴포넌트 삭제 및 NavSheet 개선

--- src/layouts/header/components/NavSheet.tsx
+++ src/layouts/header/components/NavSheet.tsx
@@ -1,4 +1,12 @@
-import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import MenuIcon from "@mui/icons-material/Menu";
 import { BOTTOM_MENU_ITEMS, TOP_MENU_ITEMS } from "./Gnb";
 import { Link } from "react-router-dom";
@@ -20,12 +28,13 @@ const NavSheet = () => {
       <SheetContent>
         <SheetHeader>
           <SheetTitle>바로가기</SheetTitle>
-          <SheetDescription>
+          <SheetDescription>사이트맵 링크를 제공합니다.</SheetDescription>
+          <nav className="py-5">
             <ul>
               {BOTTOM_MENU_ITEMS.map((item, index) => (
                 <li key={index} className="text-center">
                   <SheetClose asChild>
-                    <Link className="block p-2 text-3xl font-bold transition-colors duration-100 hover:text-fire" to={item.path}>
+                    <Link className="block p-2 duration-100 hover:font-semibold" to={item.path}>
                       {capitalizeFirstLetter(item.name)}
                     </Link>
                   </SheetClose>
@@ -34,23 +43,25 @@ const NavSheet = () => {
               {TOP_MENU_ITEMS.map((item, index) => (
                 <li key={index} className="text-center">
                   <SheetClose asChild>
-                    <Link className="block p-2 text-3xl font-bold transition-colors duration-100 hover:text-fire" to={item.path}>
+                    <Link className="block p-2 duration-100 hover:font-semibold" to={item.path}>
                       {capitalizeFirstLetter(item.name)}
                     </Link>
                   </SheetClose>
                 </li>
               ))}
             </ul>
-            <SheetClose asChild className="absolute bottom-0 right-0 p-2">
-              <div>
-                {!isAdminAuthenticated() ? (
-                  <Link to="/admin">유저 모드</Link> // 인증되지 않은 경우
-                ) : (
-                  <button onClick={clearAdminAuthCookie}>관리자 모드</button> // 인증된 경우
-                )}
-              </div>
-            </SheetClose>
-          </SheetDescription>
+          </nav>
+          <SheetClose asChild className="absolute bottom-0 right-0 p-2">
+            {!isAdminAuthenticated() ? (
+              <Link to="/admin" className="text-xs text-gray-500">
+                유저 모드
+              </Link> // 인증되지 않은 경우
+            ) : (
+              <button onClick={clearAdminAuthCookie} className="text-xs text-gray-500">
+                관리자 모드
+              </button> // 인증된 경우
+            )}
+          </SheetClose>
         </SheetHeader>
       </SheetContent>
     </Sheet>
