e0218970affdc3421ebd282338d342e64ca6e31f
miss-gif <svx327@naver.com>
2024-12-09 00:14:53 +0900

    Feat: 자주 묻는 질문 목록 추가 및 인터뷰 탭 컴포넌트 수정

--- src/features/Interview/components/Interview/Interview-Tabs.tsx
+++ src/features/Interview/components/Interview/Interview-Tabs.tsx
@@ -3,7 +3,7 @@ import { TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const InterviewTabs = () => {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between" id="link-point">
       <TabsList className="grid w-[400px] grid-cols-2">
         <TabsTrigger value="answered">답변 된 질문</TabsTrigger>
         <TabsTrigger value="unanswered">답변을 기다리는 질문</TabsTrigger>
