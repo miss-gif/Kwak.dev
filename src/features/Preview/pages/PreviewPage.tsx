49316c23b83f1dc2de16248f80fe3d2787d062a3
miss-gif <svx327@naver.com>
2025-01-06 23:21:59 +0900

    Refactor: PreviewPage 컴포넌트에서 뒤로 가기 버튼 클릭 핸들러를 분리하여 코드 가독성 향상

--- src/features/Preview/pages/PreviewPage.tsx
+++ src/features/Preview/pages/PreviewPage.tsx
@@ -5,14 +5,13 @@ import Preview from "../components";
 const PreviewPage = () => {
   const navigate = useNavigate();
 
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <>
-      <Button
-        className="fixed right-0 top-0 z-50 m-5 border border-gray-500 text-xs"
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
+      <Button className="fixed right-0 top-0 z-50 m-5 border border-gray-500 text-xs" onClick={handleBackClick}>
         종료
       </Button>
       <Preview />
