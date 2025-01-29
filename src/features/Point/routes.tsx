6c1d3cf17ed5c5a77cd3ce53fdbeaab783811ea6
miss-gif <svx327@naver.com>
2025-01-29 22:42:18 +0900

    Refactor: 포인트 페이지에 보호된 경로 추가

--- src/features/Point/routes.tsx
+++ src/features/Point/routes.tsx
@@ -1,5 +1,15 @@
+import ProtectedRoute from "@/routes/ProtectedRoute";
 import { lazy } from "react";
 
 const PointPage = lazy(() => import("./pages/PointPage"));
 
-export const pointRoutes = [{ path: "/point", element: <PointPage /> }];
+export const pointRoutes = [
+  {
+    path: "/point",
+    element: (
+      <ProtectedRoute>
+        <PointPage />
+      </ProtectedRoute>
+    ),
+  },
+];
